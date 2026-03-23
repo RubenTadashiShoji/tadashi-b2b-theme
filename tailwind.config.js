f05b6e575fde341b228e298450599b0bbc4dfc69
Ruben Epong <ruben.epong@tadashishoji.com>
2026-03-22 21:58:07 -0700

    turned off tailwind css preset

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: 'tw-',
+  corePlugins: {
+    preflight: false
+  },
+  content: [
+    "./layout/*.liquid",
+    "./templates/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid"
+  ],
+}
