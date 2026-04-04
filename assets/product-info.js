51d2928cfdb8addd5944cbde1f68d48bc398651f
Ruben Epong <rubenjohne@gmail.com>
2026-04-04 08:58:52 -0700

    feat(product): add Color block to display selected variant color value

--- assets/product-info.js
+++ assets/product-info.js
@@ -188,6 +188,8 @@ if (!customElements.get('product-info')) {
 
           updateSourceFromDestination('price');
           updateSourceFromDestination('Sku', ({ classList }) => classList.contains('hidden'));
+          updateSourceFromDestination('StyleSku');
+          updateSourceFromDestination('Color');
           updateSourceFromDestination('Inventory', ({ innerText }) => innerText === '');
           updateSourceFromDestination('Volume');
           updateSourceFromDestination('Price-Per-Item', ({ classList }) => classList.contains('hidden'));
